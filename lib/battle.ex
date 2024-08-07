1d943b6b3f2163acdd0e6e7979d202937471628c
靑鲤 <fengweizhao.fwz@alibaba-inc.com>
2024-08-07 14:58:33 +0800

    first_commitd

---
+++ lib/battle.ex
@@ -0,0 +1,18 @@
+defmodule Battle do
+  @moduledoc """
+  Documentation for `Battle`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Battle.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
