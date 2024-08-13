18ce9f077c02c3ee77bbc6837e17475ee67936cb
靑鲤 <fengweizhao.fwz@alibaba-inc.com>
2024-08-13 19:49:18 +0800

    before launch kun

--- lib/battle/mongo/user_ai.ex
+++ lib/battle/mongo/user_ai.ex
@@ -51,6 +51,8 @@ defmodule Battle.Mongo.UserAi do
 
     info = %{user_id: user_id, ai_name: ai_name, git_url: git_url, tag: tag,create_time: Ejoy.Bson.utc_now()}
     __MODULE__.psave(info)
+
+
 #UserAi.insert_ai(1,"Biu","git.com","1.0")
 #UserAi.get_ai_list_by_userId(1)
 
--- lib/battle/service/battle_service/room_supervisor.ex
+++ lib/battle/service/battle_service/room_supervisor.ex
@@ -24,52 +24,53 @@ defmodule Battle.Service.BattleService.RoomSupervisor do
   end
 
   def join(moment_token) do
+
     Logger.info(moment_token)
-#    {:ok,user_info} = Token.verify_token_battle(moment_token)
-#    user_id = user_info.user_id
-#    contest_id = user_info.ext.account_id
+    {:ok,user_info} = Token.verify_token_battle(moment_token)
+    user_id = user_info.user_id
+    contest_id = user_info.ext.account_id
 
-#    case Registry.lookup(Battle.RoomRegistry,contest_id) do
-#      [{pid,_}] ->
-#        RoomServer.add_player(pid,user_id)
-#      [] ->
-#        {:error, "room not found"}
-#    end
+    case Registry.lookup(Battle.RoomRegistry,contest_id) do
+      [{pid,_}] ->
+        RoomServer.add_player(pid,user_id)
+      [] ->
+        {:error, "room not found"}
+    end
 
-    detail = %{
-      code: 100,
-      black: "user_id_1",
-      white: "user_id_2"
-    }
-
-    {:ok,detail}
+#    detail = %{
+#      code: 100,
+#      black: "user_id_1",
+#      white: "user_id_2"
+#    }
+#
+#    {:ok,detail}
   end
 
   def battle_handler(handle_detail,moment_token) do
     [[x0, y0], [x1, y1]] = Jason.decode!(handle_detail)
-    Logger.info("#{x0}<>   <>#{y0}<>   <>#{x1}<>   <>#{y1}")
-    Logger.info(moment_token)
+#    Logger.info("#{x0}<>   <>#{y0}<>   <>#{x1}<>   <>#{y1}")
+#    Logger.info(moment_token)
 
-#    {:ok,user_info} = Token.verify_token_battle(moment_token)
-##    user_id = user_info.user_id
-#    contest_id = user_info.ext.account_id
-#
-##    case Registry.lookup(Battle.RoomRegistry,contest_id) do
-##      [{pid,_}] ->
-##        RoomServer.movement(pid,x0,y0,x1,y1)
-##      [] ->
-##        {:error, "room not found"}
-##    end
+    {:ok,user_info} = Token.verify_token_battle(moment_token)
+    user_id = user_info.user_id
+    contest_id = user_info.ext.account_id
 
-    detail = %{
-      code: "init",
-      winner: "",
-      white_king: "user_id_2",
-      black_king: "user_id_1",
-      opponent_step: [[1,1],[1,3]],
-      captured: [[1,2]]
-    }
-    {:ok,detail}
+    case Registry.lookup(Battle.RoomRegistry,contest_id) do
+      [{pid,_}] ->
+        RoomServer.movement(pid,x0,y0,x1,y1)
+      [] ->
+        {:error, "room not found"}
+    end
+
+#    detail = %{
+#      code: "init",
+#      winner: "",
+#      white_king: "user_id_2",
+#      black_king: "user_id_1",
+#      opponent_step: [[1,1],[1,3]],
+#      captured: [[1,2]]
+#    }
+#    {:ok,detail}
   end
 end
 
