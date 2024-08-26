9217c1c8189abd6e802af14a17cabe575befae07
乙火 <dustin.ljl@alibaba-inc.com>
2024-08-26 20:23:56 +0800

    联调第一版”

--- lib/battle/mongo/user.ex
+++ lib/battle/mongo/user.ex
@@ -10,14 +10,14 @@ defmodule Battle.Mongo.User do
   @cleanable false
 
   field :account, :integer, required: true
-  field :user_id, :integer, required: true
+  field :user_id, :string, required: true
   field :date, :datetime, required: true
   field :avatar, :string, required: false
 
   def save_user(user_id, account) do
     info = %{
     user_id: user_id,
-    account: account,
+    account: String.to_integer(account),
     date: Ejoy.Bson.utc_now()
     }
     __MODULE__.psave(info)
@@ -26,7 +26,7 @@ defmodule Battle.Mongo.User do
   def query_user(user_id) do
     case __MODULE__.pquery2(%{user_id: user_id},expected_explain: %Mongo2.ExpectedExplain{indexes_plan: [[user_id: 1]]}) do
       [] -> {:error, "user id error"}
-      res -> {:ok,res|>Enum.map(fn message -> message|> __MODULE__.to_raw() end)|>List.first()}
+      res -> {:ok,res|>Enum.map(fn message -> message|> __MODULE__.to_raw() end) |> List.first()}
     end
   end
 
