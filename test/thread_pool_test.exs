773f159a9cb4559ab809e3ec347a15a78bd30983
乙火 <dustin.ljl@alibaba-inc.com>
2024-09-05 17:09:15 +0800

    单测修改

--- test/thread_pool_test.exs
+++ test/thread_pool_test.exs
@@ -5,49 +5,53 @@ defmodule BattleTest.ThreadPoolTest do
   alias Battle.Service.BattleService.ThreadPool
   alias Battle.Service.BattleService.RoomServer
 
-  setup do
-    {:ok, pid} = ThreadPool.start_link(10)
-    :ok
-  end
-
   test "starts with an initial state" do
     state = ThreadPool.get_state()
-    assert state.size == 10
     assert state.workers == []
     assert state.queue == :queue.new()
     assert state.busy == %{}
   end
 
   test "add tasks" do
-    task = {"user_id1", "user_id2", "game_id", %{"user_id1" => "plat1024-battle-players:20240904160251", "user_id2" => "plat1024-battle-players:20240904160251"}}
-    state = ThreadPool.get_state()
-    assert :ok = ThreadPool.add_task(task)
-    :timer.sleep(5000)
-    [{pid, _}] = Registry.lookup(Battle.RoomRegistry, "game_id")
-    server_state = RoomServer.get_state(pid)
-    send(ThreadPool, {:terminate, "game_id", server_state.group_name, server_state.group_key, server_state.app_name})
-    :timer.sleep(2000)
-    assert state.workers == []
+    with_mock Battle.Service.WebService.Kun, [:passthrough], [
+      create_deploy_task: fn _ -> {:ok, "create_deploy"} end,
+      create_uninstall_task: fn _ -> {:ok, "create_uninstall"} end,
+      update_service_group: fn _, _, _ -> {:ok, "update params"} end
+    ] do
+      task = {"user_id1", "user_id2", "game_id", %{"user_id1" => "plat1024-battle-players:20240904160251", "user_id2" => "plat1024-battle-players:20240904160251"}}
+      assert :ok = ThreadPool.add_task(task)
+      :timer.sleep(1000)
+      state = ThreadPool.get_state()
+      assert length(state.workers) == 1
+      [{pid, _}] = Registry.lookup(Battle.RoomRegistry, "game_id")
+      server_state = RoomServer.get_state(pid)
+      send(ThreadPool, {:terminate, "game_id", server_state.group_name, server_state.group_key, server_state.app_name})
+      state = ThreadPool.get_state()
+      assert state.workers == []
+    end
+
   end
 
   test "add task when there is no services " do
-    task = {"user_id1", "user_id2", "game_id", %{"user_id1" => "plat1024-battle-players:20240904160251", "user_id2" => "plat1024-battle-players:20240904160251"}}
-    :ets.delete_all_objects(:services_info)
-    assert :ok = ThreadPool.add_task(task)
-    :timer.sleep(1000)
-    state = ThreadPool.get_state()
-    assert :queue.len(state.queue) == 1
+    with_mock Battle.Service.WebService.Kun, [:passthrough], [
+      create_deploy_task: fn _ -> {:ok, "create_deploy"} end,
+      create_uninstall_task: fn _ -> {:ok, "create_uninstall"} end,
+      update_service_group: fn _, _, _ -> {:ok, "update params"} end
+    ] do
+      task = {"user_id1", "user_id2", "game_id", %{"user_id1" => "plat1024-battle-players:20240904160251", "user_id2" => "plat1024-battle-players:20240904160251"}}
+      :ets.delete_all_objects(:services_info)
+      assert :ok = ThreadPool.add_task(task)
+      state = ThreadPool.get_state()
+      assert :queue.len(state.queue) == 1
 
-    :ets.insert(:services_info, {"battle-players1", "battle-player-python"})
-    assert :ok = ThreadPool.add_task(task)
-    :timer.sleep(5000)
-    send(ThreadPool, {:terminate, "game_id", "battle-players1", "plat1024-players1", "battle-player-python"})
-    :timer.sleep(2000)
-    send(ThreadPool, {:terminate, "game_id", "battle-players1", "plat1024-players1", "battle-player-python"})
-    :timer.sleep(2000)
-    state = ThreadPool.get_state()
-    assert :queue.len(state.queue) == 0
-    assert state.workers == []
+      :ets.insert(:services_info, {"battle-players1", "battle-player-python"})
+      assert :ok = ThreadPool.add_task(task)
+      send(ThreadPool, {:terminate, "game_id", "battle-players1", "plat1024-players1", "battle-player-python"})
+      send(ThreadPool, {:terminate, "game_id", "battle-players1", "plat1024-players1", "battle-player-python"})
+      state = ThreadPool.get_state()
+      assert :queue.len(state.queue) == 0
+      assert state.workers == []
+    end
   end
 
   test "handle_info/2 {ref, result}" do
