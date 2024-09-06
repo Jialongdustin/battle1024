165abd203d02b49109b7253c21c801cef897dfc1
乙火 <dustin.ljl@alibaba-inc.com>
2024-09-06 15:52:52 +0800

    单测补充

--- test/tournament_test.exs
+++ test/tournament_test.exs
@@ -3,6 +3,7 @@ defmodule BattleTest.TournamentTest do
   import Mock
 
   alias Battle.Service.BattleService.Tournament
+  alias Battle.Mongo.RankList
 
   test "schedules tournament today" do
     now = DateTime.utc_now()
@@ -82,6 +83,7 @@ defmodule BattleTest.TournamentTest do
         end
       ] do
           result = Tournament.update_win_rate(games)
+          RankList.remove_rank("111")
           assert length(result) == 1
       end
     end
